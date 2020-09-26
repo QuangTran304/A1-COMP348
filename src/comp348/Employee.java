ae7b97fd827e43b4992071805214af726b769274
Quang Tran <nhatquang304@gmail.com>
2020-09-26 08:43:56 -0400

    update BigDecimal salary in Employee class.

--- src/comp348/Employee.java
+++ src/comp348/Employee.java
@@ -1,17 +1,25 @@
 package comp348;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.NumberFormat;
 
 public class Employee implements Person{
     String firstName, lastName, id;
-    double salary; //I couldn't declare a variable in BigDecimal without assigning a value!!
+    BigDecimal salary;
 
-    Employee(String id, String fname, String lname, double salary){
-        this.id = id;
-        this.firstName = fname;
-        this.lastName = lname;
-        this.salary = salary;
+    Employee(String id, String fname, String lname, BigDecimal salary){
+
+        // ID length needs to be 7 digits
+        if (id.length() == 7 ) {
+            this.id = id;
+            this.firstName = fname;
+            this.lastName = lname;
+            this.salary = salary;
+        } else {
+            System.out.println("Error: Employee's ID needs to be a 7-digit string");
+            System.exit(1);
+        }
     }
 
     @Override
@@ -24,10 +32,7 @@ public class Employee implements Person{
         return this.firstName + " " + this.lastName;
     }
 
-    public BigDecimal getSalary(){
-        BigDecimal returnSalray = BigDecimal.valueOf(salary);
-        return returnSalray;
-    }
+    public BigDecimal getSalary(){ return salary; }
 
     public String toString(){
         NumberFormat salaryInDollar = NumberFormat.getCurrencyInstance();
@@ -37,14 +42,14 @@ public class Employee implements Person{
                 + salaryInDollar.format(this.salary);
     }
 
-    static Employee parse(String input){
-        String[] inputs = new String[4];
-        inputs = input.split(",");
-        Employee newEmployee = new Employee(inputs[0],
-                                            inputs[1],
-                                            inputs[2],
-                                            Double.parseDouble(inputs[3]));
-        return newEmployee;
-    }
+//    static Employee parse(String input){
+//        String[] inputs = new String[4];
+//        inputs = input.split(",");
+//        Employee newEmployee = new Employee(inputs[0],
+//                                            inputs[1],
+//                                            inputs[2],
+//                                            Double.parseDouble(inputs[3]));
+//        return newEmployee;
+//    }
 
 }
--- src/comp348/Main.java
+++ src/comp348/Main.java
@@ -1,37 +1,22 @@
 package comp348;
 
-import static comp348.Employee.parse;
+
+import java.math.BigDecimal;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!!");
-
-
-
-        // Write anything here => Try to 'pull' (VCS -> Git -> Pull...), 'commit' and 'push' to remote repository!!
-        System.out.println("testing");
-
-
-
-
-        // Write anything here => Try to 'pull' (VCS -> Git -> Pull...), 'commit' and 'push' to remote repository!!
-
-        System.out.println("testing eric...");
-
-
-        // Write anything here => Try to 'pull' (VCS -> Git -> Pull...), 'commit' and 'push' to remote repository!!
-
-
-        // Write anything here => Try to 'pull' (VCS -> Git -> Pull...), 'commit' and 'push' to remote repository!!
-
-
         //input test (by parsa)
-        Employee e1 = parse("0132435,John,Doe,30000.00");
-        System.out.println(e1.toString());
-        System.out.println(e1.getSalary());
-        System.out.println(e1.getID());
-        System.out.println(e1.getName());
+//        Employee e1 = e1.parse("0132435,John,Doe,30000.00");
+//        System.out.println(e1.toString());
+//        System.out.println(e1.getSalary());
+//        System.out.println(e1.getID());
+//        System.out.println(e1.getName());
+
+        // Test by Quang
+        Person worker1 = new Employee("1234567", "John", "Doe", new BigDecimal("3000.02"));
+        System.out.println(worker1.toString());
+
     }
 }
